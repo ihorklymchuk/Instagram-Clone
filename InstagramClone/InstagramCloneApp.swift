a449a027553add0a1b190a00bf5381e6e25a2831
Ihor Klymchuk <ihorklymchuk1918@gmail.com>
2022-10-28 16:07:11 +0200

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Ihor Klymchuk on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
